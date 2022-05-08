73b93e82f2db3e35581d7efe1d9f390d731a6b69
Jeyoung-Park <jason9380@naver.com>
2022-05-09 00:18:46 +0900

    fix: crud 컨트롤러 파일

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
 const Koa = require('koa');
 const Router = require('koa-router');
+const bodyParser = require('koa-bodyparser');
 
 const PORT = 4000;
 
+// 라우터 설정
 const api = require('./api');
 
 const app = new Koa();
 const router = new Router();
+
+// 라우터 적용 저넹 bodyParser 적용
+app.use(bodyParser());
 
 // 라우터 설정
 router.use('/api', api.routes());
